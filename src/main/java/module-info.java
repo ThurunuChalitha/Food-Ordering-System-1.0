22f7a1fa9d798f1aa38ccb98284d2ef8e3fd0fb6
Dushyantha <dushyantha1208@gmail.com>
2022-01-27 22:31:40 +0530

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.grp2.foodorderingsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.grp2.foodorderingsystem to javafx.fxml;
+    exports com.grp2.foodorderingsystem;
+}
