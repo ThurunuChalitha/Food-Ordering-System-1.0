7ac7c43ca6f151476ff931e853df8cf9fec0b293
Bhathiya Bandara <bhathiyaakalanka1997@gmail.com>
2022-03-05 22:09:49 +0530

    Bhathiya

---
+++ src/main/java/com/grp2/foodorderingsystem/BinarySearch_gp.java
@@ -0,0 +1,30 @@
+package binarysearch_gp;
+
+public class BinarySearch_gp {
+    
+    public static int binarySearch(int arr[], int left, int right, int key){  
+        if (right>= left){  
+            int mid= left + (right - left)/2;  
+            if (arr[mid]== key){  
+                return mid;  
+            }if (arr[mid]> key){  
+                return binarySearch(arr, left, mid-1, key); 
+            }else{  
+                return binarySearch(arr, mid+1, right, key);
+            }  
+        }  
+        return -1;  
+    }  
+    public static void main(String args[]){  
+        int arr[]= {10,20,30,40,50,60,70,80,90,100};  
+        int key= 90;  
+        int right=arr.length-1;  
+        int result= binarySearch(arr,0,right,key);  
+        
+        if (result== -1){ 
+            System.out.println("Element is not found!");  
+        }else{  
+            System.out.println("Element is found at index: "+ result);
+        }
+    }  
+}
