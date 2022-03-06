2c51126e5521ad6693c80cda2cd6f700adb3e9aa
Tharindu Buwaneka <buwanekatharindu@gmail.com>
2022-03-07 02:27:34 +0530

    Created newItemSceneController.java

---
+++ src/main/java/com/grp2/foodorderingsystem/newItemSceneController.java
@@ -0,0 +1,35 @@
+package com.grp2.foodorderingsystem;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class newItemSceneController {
+
+    @FXML
+    private TextField newFoodName;
+
+    @FXML
+    private TextField newFoodCode;
+
+    @FXML
+    private TextField newFoodCount;
+
+    @FXML
+    private TextField unitPrice;
+
+    @FXML
+    private TextField processTime;
+
+    @FXML
+    private ChoiceBox<String> cmbFoodType;
+
+    @FXML
+    public void btnAddNewItem(ActionEvent event){
+        
+
+    }
+
+
+}
